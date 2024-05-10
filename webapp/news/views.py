603cbef4de6790b7b4d1f25a5ec61788eaf40565
Vitalitch <serovvitalx@gmail.com>
2024-05-10 17:44:59 +0600

    Editing the appearance of the site, adding an admin panel, outputting from the database

---
+++ webapp/news/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from .models import Article
+
+
+def news_home(request):
+    news = Article.objects.order_by('-date')
+    return render(request,'news/news_home.html', {'news': news})
